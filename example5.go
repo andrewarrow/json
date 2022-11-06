099ee0a08c4404b81c7502ca7cd406091905d8de
Andrew Arrow <oneone@gmail.com>
2022-11-06 05:52:44 -0800

    example5 showing thumbnail width x height

--- example5.go
+++ example5.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"html"
 	"json/files"
 )
 
@@ -15,18 +16,33 @@ type Item struct {
 }
 
 type Snippet struct {
-	Title string
+	Title      string
+	Thumbnails Thumbnails
+}
+
+type Thumbnails struct {
+	Default Thumbnail
+	Medium  Thumbnail
+	High    Thumbnail
+}
+
+type Thumbnail struct {
+	Url    string `json:"url"`
+	Width  int    `json:"width"`
+	Height int    `json:"height"`
 }
 
 func Example5() {
 	jsonString := files.ReadFile("examples/005_youtube.json")
-	fmt.Println(jsonString)
 
 	yt := Youtube{}
 	json.Unmarshal([]byte(jsonString), &yt)
 	fmt.Println("The length is:", len(yt.Items))
 	for i, item := range yt.Items {
-		fmt.Println(i, item.Snippet.Title)
-		//fmt.Println(i, html.UnescapeString(item.Snippet.Title))
+		fmt.Println(i, html.UnescapeString(item.Snippet.Title))
+		fmt.Print(item.Snippet.Thumbnails.Default.Width)
+		fmt.Print("x")
+		fmt.Println(item.Snippet.Thumbnails.Default.Height)
+		fmt.Println("")
 	}
 }
