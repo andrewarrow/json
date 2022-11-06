70d58c7e13d49038ee10bb6620fdb8c076ef67c5
Andrew Arrow <oneone@gmail.com>
2022-11-06 04:39:34 -0800

    example2 ready to parse

--- example2.go
+++ example2.go
@@ -5,7 +5,15 @@ import (
 	"json/files"
 )
 
+type SimpleList struct {
+	Items []string
+}
+
 func Example2() {
 	jsonString := files.ReadFile("examples/002_simple_list.json")
 	fmt.Println(jsonString)
+
+	sl := SimpleList{}
+	fmt.Println("The simple list is:", sl.Items)
+	fmt.Println("Thelength is:", len(sl.Items))
 }
