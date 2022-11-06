e276235bef1adf04804e23b3cebd52659183a5c3
Andrew Arrow <oneone@gmail.com>
2022-11-06 04:32:21 -0800

    setting up a struct to hold our message

--- example1.go
+++ example1.go
@@ -5,7 +5,15 @@ import (
 	"json/files"
 )
 
+type Message struct {
+	Message string
+}
+
 func Example1() {
 	jsonString := files.ReadFile("examples/001_say_hi.json")
 	fmt.Println(jsonString)
+
+	m := Message{}
+	fmt.Println("The message is:", m.Message)
+	fmt.Println("The message length is:", len(m.Message))
 }
